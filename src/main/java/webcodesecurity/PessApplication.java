2fe45e21c77d00904e204b29dfc2a1f5e06b5676
Yeojin0523 <d02jyjeong@naver.com>
2025-05-19 20:32:47 +0900

    프론트-백 초기설정

---
+++ src/main/java/webcodesecurity/PessApplication.java
@@ -0,0 +1,13 @@
+package webcodesecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PessApplication.class, args);
+	}
+
+}
